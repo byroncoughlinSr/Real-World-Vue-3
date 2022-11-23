669db4da1ecdad5d1bc50333e6b57c7cc3f962eb
Byron Coughlin <byroncoughlinsr@gmail.com>
2022-11-23 04:51:15 -0800

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link :to="{ name: 'EventList' }">Events</router-link> |
+    <router-link :to="{ name: 'About' }">About</router-link>
   </nav>
+
   <router-view />
 </template>
 
@@ -26,5 +27,9 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+h4 {
+  font-size: 20 px;
 }
 </style>
---
+++ src/components/prettierrc.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true,
+  semiQuote: true,
+  semi: false,
+};
